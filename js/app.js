80838ee521774c7afbd549245653f8e32d122e72
brandon <josueknd@hotmail.es>
2015-03-19 08:51:11 -0600

    proyect jasmine finish

--- js/app.js
+++ js/app.js
@@ -64,6 +64,8 @@ function loadFeed(id, cb) {
             title.html(feedName);   // Set the header text
             container.empty();      // Empty out all previous entries
 
+            debugger;
+
             /* Loop through the entries we just loaded via the Google
              * Feed Reader API. We'll then parse that entry against the
              * entryTemplate (created above using Handlebars) and append
